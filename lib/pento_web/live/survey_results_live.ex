103517317ff0c91fa257201edd6d54e65c957dae
Jack Lansing <jack.lansing@live.com>
2021-03-16 15:25:59 -0400

    add bar chart macro, setup survey component with age filter

--- lib/pento_web/live/survey_results_live.ex
+++ lib/pento_web/live/survey_results_live.ex
@@ -1,13 +1,30 @@
 defmodule PentoWeb.SurveyResultsLive do
   use PentoWeb, :live_component
+  use PentoWeb, :chart_live
   alias Pento.Catalog
-  alias Contex.Plot
 
   def update(assigns, socket) do
     {
       :ok,
       socket
       |> assign(assigns)
+      |> assign_age_group_filter()
+      |> assign_products_with_average_ratings()
+      |> assign_dataset()
+      |> assign_chart()
+      |> assign_chart_svg()
+    }
+  end
+
+  def handle_event(
+        "age_group_filter",
+        %{"age_group_filter" => age_group_filter},
+        socket
+      ) do
+    {
+      :noreply,
+      socket
+      |> assign_age_group_filter(age_group_filter)
       |> assign_products_with_average_ratings()
       |> assign_dataset()
       |> assign_chart()
@@ -31,14 +48,7 @@ defmodule PentoWeb.SurveyResultsLive do
 
   def assign_chart_svg(%{assigns: %{chart: chart}} = socket) do
     socket
-    |> assign(:chart_svg, render_bar_chart(chart))
-  end
-
-  defp render_bar_chart(chart) do
-    Plot.new(500, 400, chart)
-    |> Plot.titles(title(), subtitle())
-    |> Plot.axis_labels(x_axis(), y_axis())
-    |> Plot.to_svg()
+    |> assign(:chart_svg, render_bar_chart(chart, title(), subtitle(), x_axis(), y_axis()))
   end
 
   defp title, do: "Product Ratings"
@@ -51,19 +61,29 @@ defmodule PentoWeb.SurveyResultsLive do
     |> assign(:chart, make_bar_chart(dataset))
   end
 
-  defp make_bar_chart_dataset(data) do
-    Contex.Dataset.new(data)
-  end
-
-  defp make_bar_chart(dataset) do
-    Contex.BarChart.new(dataset)
-  end
-
-  defp assign_products_with_average_ratings(socket) do
+  defp assign_products_with_average_ratings(
+         %{assigns: %{age_group_filter: age_group_filter}} = socket
+       ) do
     socket
     |> assign(
       :products_with_average_ratings,
-      Catalog.products_with_average_ratings()
+      get_products_with_average_ratings(%{age_group_filter: age_group_filter})
     )
+  end
+
+  defp get_products_with_average_ratings(filter) do
+    case Catalog.products_with_average_ratings(filter) do
+      [] -> Catalog.products_with_zero_ratings()
+      products -> products
+    end
+  end
+
+  defp assign_age_group_filter(socket) do
+    socket
+    |> assign(:age_group_filter, "all")
+  end
+
+  defp assign_age_group_filter(socket, age_group_filter) do
+    assign(socket, :age_group_filter, age_group_filter)
   end
 end
